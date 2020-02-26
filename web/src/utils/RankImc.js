8f5c8edc42afd3bb69ac2b679100cc7073184cbd
Diego da Silva Lourenço <diego.silva147@gmail.com>
2020-02-26 12:43:29 -0300

    Util RankImc

---
+++ web/src/utils/RankImc.js
@@ -0,0 +1,42 @@
+export default function RankImc(name, imc, weight) {
+  const success = "success";
+  const danger = "danger";
+
+  if (imc < 18.5) {
+    let idealWeight = 62 - weight;
+    return {
+      active: danger,
+      rank: "thinness",
+      message: `Sinto muito ${name}, mas o seu IMC é de ${imc} kg/m2, por isso você está abaixo 
+        do peso e deveria engordar no mínimo ${idealWeight} kg.`
+    };
+  }
+
+  if (imc >= 18.5 && imc <= 24.9) {
+    return {
+      active: success,
+      rank: "normal",
+      message: `Parabéns ${name}, seu IMC é ${imc} kg/m2 e você está dentro do peso adequado!`
+    };
+  }
+
+  if (imc > 24.9 && imc <= 30) {
+    let idealWeight = weight - 83;
+    return {
+      active: danger,
+      rank: "overweight",
+      message: `Sinto muito ${name}, mas o seu IMC é de ${imc} kg/m2, por isso você está
+        acima do peso e deveria emagrecer no mínimo ${idealWeight} Kg.`
+    };
+  }
+
+  if (imc > 30) {
+    let idealWeight = weight - 83;
+    return {
+      active: danger,
+      rank: "obesity",
+      message: `Sinto muito ${name}, mas o seu IMC é de ${imc} kg/m2, por isso você está
+      muito acima do peso, você deveria emagrecer ${idealWeight} Kg.`
+    };
+  }
+}
