be9981a15b0b11e68f3a3cf99c1968bfaa725cab
Diego Lourenço <diego.silva147@gmail.com>
2020-02-28 15:09:34 -0300

    Update screenOptions Stack Navigator

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
@@ -9,7 +10,15 @@ const Stack = createStackNavigator();
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerBackTitleVisible: false,
+          headerTitleAlign: "center",
+          headerTitleStyle: {
+            fontWeight: "bold"
+          }
+        }}
+      >
         <Stack.Screen name="Calculadora IMC" component={Form} />
         <Stack.Screen name="Resultado" component={Result} />
       </Stack.Navigator>
